44df219ffd1f92d5e4d370e74d1cb8b3db738750
Kirk <bardini.kirk@gmail.com>
2021-02-16 22:33:32 -0300

    Lesson 5

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '888883',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
